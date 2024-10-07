b0e8db6db52ce08e81cd704d2e5e192f5e11f5f7
Rokidna G Mac V <qq617462323@outlook.com>
2024-10-07 10:34:55 +0800

    add 2nd activity

--- app/src/main/java/com/gnetwork/playground/MainActivity.java
+++ app/src/main/java/com/gnetwork/playground/MainActivity.java
@@ -1,6 +1,9 @@
 package com.gnetwork.playground;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.tv);
         tv.setText("你好，世界！");
+
+        Button button =  findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
