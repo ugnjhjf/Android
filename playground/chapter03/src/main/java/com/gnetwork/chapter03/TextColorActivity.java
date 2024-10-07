ee36b2cbe1e753f122d951c4bfd39e782001219e
Rokidna G Mac V <qq617462323@outlook.com>
2024-10-07 16:16:30 +0800

    Text color changed

---
+++ playground/chapter03/src/main/java/com/gnetwork/chapter03/TextColorActivity.java
@@ -0,0 +1,20 @@
+package com.gnetwork.chapter03;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TextColorActivity extends AppCompatActivity {
+    @Override
+    // onCreate() 方法是 Activity 的生命周期方法，当 Activity 创建时会调用这个方法
+    //创建 Activity 实例 -> 寻找需要显示的元素 -> 设置元素的属性 -> 显示元素
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_view);
+        TextView tv_code_system = findViewById(R.id.tv_code_system);
+        tv_code_system.setTextColor(Color.GREEN);
+    }
+}
