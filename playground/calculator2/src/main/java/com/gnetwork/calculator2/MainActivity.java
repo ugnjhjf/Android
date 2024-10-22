5a2d65b504f9b9d2eff1df9ed52d17b248d54f5b
Rokidna G Mac V <qq617462323@outlook.com>
2024-10-22 15:08:42 +0800

    Fix crash bug

--- playground/calculator2/src/main/java/com/gnetwork/calculator2/MainActivity.java
+++ playground/calculator2/src/main/java/com/gnetwork/calculator2/MainActivity.java
@@ -66,15 +66,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
               break;
 
             case R.id.btn_cancel:
-                firstNum = "";
-                secondNum = "";
-                operator = "";
+                // 如果操作符为空，表示正在输入的是第一个操作数，清除 firstNum
+                if (operator.isEmpty()) {
+                    firstNum = "";
+                } else {
+                    // 否则清除第二个操作数
+                    secondNum = "";
+                }
                 result = "";
-                showText = "";
+                showText = showText.substring(0, showText.length() - 1);  // 从显示内容中去掉最后一位
                 refreshText(showText);
-
-
                 break;
+
 
             case R.id.btn_plus:
             case R.id.btn_subtract:
@@ -95,10 +98,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 refreshText(showText + "√=" + result);
                 break;
             case R.id.btn_reciprocal:
-                double reciprocal_result = 1.0 / Double.parseDouble(firstNum);
-                refreshOperate(String.valueOf(reciprocal_result));
-                refreshText(showText + "/=" + reciprocal_result);
+                if (firstNum.isEmpty() || Double.parseDouble(firstNum) == 0) {
+                    refreshText("Cannot divide by zero");
+                    clear(); // 重置计算器
+                } else {
+                    double reciprocal_result = 1.0 / Double.parseDouble(firstNum);
+                    refreshOperate(String.valueOf(reciprocal_result));
+                    refreshText("1/" + firstNum + "=" + reciprocal_result);
+                }
                 break;
+
 
             default:
                 if (result.length() > 0 && operator.equals("")){
@@ -128,8 +137,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return Double.parseDouble(firstNum) - Double.parseDouble(secondNum);
             case "×":
                     return Double.parseDouble(firstNum) * Double.parseDouble(secondNum);
-            default:
+            case "/":
                     return Double.parseDouble(firstNum) / Double.parseDouble(secondNum);
+            default:
+                return Double.parseDouble(firstNum);
         }
     }
 
